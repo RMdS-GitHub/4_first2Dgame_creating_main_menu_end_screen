11ca152afac822e98c8e6ebd79fa416c11e2d411
RMdS-GitHub <rmds.dev@protonmail.com>
2020-10-23 17:36:58 +0200

    Adding autoloads.

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -5,12 +5,15 @@ func _ready() -> void:
 	_velocity.x = -speed.x
 	
 	
+export var score: = 100
+	
+	
 func _on_StompDetection_body_entered(body: Node) -> void:
 	if body.global_position.y > get_node("StompDetector").global_position.y:
 		return
 	# This line under is not needed.
 	get_node("CollisionShape2D")
-	queue_free()
+	die()
 
 	
 func _physics_process(delta: float) -> void:
@@ -22,3 +25,7 @@ func _physics_process(delta: float) -> void:
 	_velocity.y = move_and_slide_with_snap(
 	_velocity, snap, FLOOR_NORMAL, true, 4, PI / 3).y
 	
+	
+func die() -> void:
+	queue_free()
+	PlayerData.score += score
